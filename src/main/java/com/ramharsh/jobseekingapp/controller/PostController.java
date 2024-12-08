b9e74b92958728d4cabe42ad40f42bddfd9e7b30
Ramharsh-aidev <ramharshdandekar@gmail.com>
2024-12-08 15:56:49 +0530

    implementated search feature

--- src/main/java/com/ramharsh/jobseekingapp/controller/PostController.java
+++ src/main/java/com/ramharsh/jobseekingapp/controller/PostController.java
@@ -1,13 +1,23 @@
 package com.ramharsh.jobseekingapp.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramharsh.jobseekingapp.modal.Post;
+import com.ramharsh.jobseekingapp.repository.PostRepository;
+import com.ramharsh.jobseekingapp.repository.SearchRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
 import springfox.documentation.annotations.ApiIgnore;
-
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
